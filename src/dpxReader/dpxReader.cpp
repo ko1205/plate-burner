833ee767899768e4350d274e7339e9fffcd9a0da
unknown <admin@RND001.2lfilm.com>
2014-11-28 23:08:23 +0900

    edit

--- src/dpxReader/dpxReader.cpp
+++ src/dpxReader/dpxReader.cpp
@@ -1,6 +1,8 @@
 #include "DPXimage_Header.h"
 #include <stdio.h>
 #include <iostream>
+#include <QApplication>
+#include <QImage>
 
 #define Endian32_Swap(value) \
         ((((U32)((value) & 0x000000FF)) << 24) | \
@@ -10,15 +12,50 @@
 
 void flip(void *);
 
-int main(int argc, char **argv){
-    FILE *f;
-    f = fopen("E:\\test_images\\dd\\a.a1.dpx","r");
-    DPXHeader dpxhd;
-    fread(&dpxhd,sizeof dpxhd,1,f);
+class DpxReader
+{
+public:
+    DpxReader();
+    ~DpxReader();
+    int getWidth();
+    int getHeight();
+private:
+    int width;
+    int height;
+};
 
-    std::cout << (char*)&dpxhd.file.magic_num << std::endl \
-        << dpxhd.file.file_size << std::endl;
-    char buff[100];
-    scanf("%s",buff);
-    return 0;
+DpxReader::DpxReader()
+{
+    FILE *f;
+    DPXHeader dpxhd;
+    f = fopen("E:\\test_images\\dd\\a.a1.dpx","r");
+    fread(&dpxhd,sizeof(dpxhd),1,f);
+    width = dpxhd.image.pixels_per_line;
+    height = dpxhd.image.lines_per_image_ele;
+}
+
+DpxReader::~DpxReader()
+{
+}
+
+int DpxReader::getWidth()
+{
+    return width;
+
+}
+
+int DpxReader::getHeight()
+{
+    return height;
+
+}
+
+
+
+int main(int argc, char **argv){
+    QApplication app(argc,argv);
+    DpxReader dpx;
+    QImage image(dpx.getWidth(),dpx.getHeight(),QImage::Format_RGB32);
+
+    return app.exec();
 }
