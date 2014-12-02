a3fc5f7e5acf161c402af6256b22ffeebb2ceeec
unknown <admin@RND001.2lfilm.com>
2014-12-02 17:50:56 +0900

    setPixel함수 memcpy함수로 변경 속도 향상 0.025 -> 0.006

--- src/dpxReader/dpxReader.cpp
+++ src/dpxReader/dpxReader.cpp
@@ -4,6 +4,8 @@
 #include <iostream>
 #include <QApplication>
 #include <QLabel>
+#include <time.h>
+#include <string.h>
 
 #define Endian32_Swap(value) \
         ((((U32)((value) & 0x000000FF)) << 24) | \
@@ -31,20 +33,7 @@ DpxReader::DpxReader()
 
 DpxReader::DpxReader(QString &path)
 {
-    FILE *f;
-    DPXHeader dpxhd;
-
-    QString Tpath = path.replace("/","\\");
-    f = fopen(Tpath.toAscii(),"r");
-    fread(&dpxhd,sizeof(dpxhd),1,f);
-    rewind(f);
-    fseek(f,dpxhd.file.offset,SEEK_SET);
-    width = dpxhd.image.pixels_per_line;
-    height = dpxhd.image.lines_per_image_ele;
-
-    bits = new unsigned int[width*height];
-    fread(bits,width*height*4,1,f);
-    fclose(f);
+    setPath(path);
 }
 
 DpxReader::~DpxReader()
@@ -86,14 +75,24 @@ int DpxReader::getHeight()
 QImage DpxReader::getQImage()
 {
     QImage image(getWidth(),getHeight(),QImage::Format_RGB32);
-    for(int y = 0; y < getHeight(); y++){
-        for(int x = 0; x < getWidth(); x++){
-            int index(y*getWidth() + x);
-            unsigned int temp = bits[index]; 
-            image.setPixel(x,y,((temp&0x00000FFC)>>4)|((temp&0x003FC000)>>6)|((temp&0xFF000000)>>8));
-        }
+    clock_t start_time,end_time;
+    start_time = clock();
+    unsigned char *buff = image.bits();
+    for(int i = 0; i < width*height;i++)
+    {
+        bits[i] = ((bits[i]&0x00000FFC)>>4)|((bits[i]&0x003FC000)>>6)|((bits[i]&0xFF000000)>>8);
     }
-    image = image.scaled(200,250,Qt::KeepAspectRatio,Qt::SmoothTransformation);
+    //for(int y = 0; y < getHeight(); y++){
+    //    for(int x = 0; x < getWidth(); x++){
+    //        int index(y*getWidth() + x);
+    //        unsigned int temp = bits[index]; 
+    //        image.setPixel(x,y,((temp&0x00000FFC)>>4)|((temp&0x003FC000)>>6)|((temp&0xFF000000)>>8));
+    //    }
+    //}
+    memcpy(buff,bits,getHeight()*getWidth()*4);
+    end_time = clock();
+    std::cout << ((double)(end_time-start_time)) / CLOCKS_PER_SEC << std::endl;
+    image = image.scaled(200,250,Qt::KeepAspectRatio,Qt::FastTransformation);
     
     return image;
 }
